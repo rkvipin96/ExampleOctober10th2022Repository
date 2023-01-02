5497ff5ccf5ee8e83f94c4ec69bf1cd9976516c0
rkvipin96 <rkvipin96@gmail.com>
2023-01-02 17:37:01 +0530

    Even number while loop new Package

---
+++ xl-Automation/src/com/sgtesting/p1/EvenNumberwhile.java
@@ -0,0 +1,17 @@
+package com.sgtesting.p1;
+
+public class EvenNumberwhile {
+
+	public static void main(String[] args) {
+		int x = 1;
+		
+		while(x<=10)
+		{
+			int num = x * 2;
+			System.out.println(num);
+			x++;
+		}
+
+	}
+
+}
