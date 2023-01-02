6c79f8a3c00792e4dfe37ac6e3166a6516a04c9a
rkvipin96 <rkvipin96@gmail.com>
2023-01-02 17:18:50 +0530

    TestNg Even Number Scenario

---
+++ xl-Automation/src/com/sgtesting/xlDemo/EvenNumber.java
@@ -0,0 +1,17 @@
+package com.sgtesting.xlDemo;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		
+		int num = 0;
+		for (int i = 1 ; i<=10 ; i++)
+		{
+			num = num + 2;
+			System.out.println(num);
+			
+		}
+
+	}
+
+}
